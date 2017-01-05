212dca70aad606d1cf1672a3757a7b3bf6ec099e
Mike Bautista <miguelraulb@gmail.com>
2017-01-04 23:06:57 -0600

    Adding install.sh installer script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing extra CPAN modules for pcap_analyzer..."
+cpan -i use Net::TcpDumpLog NetPacket::Ethernet NetPacket::IP NetPacket::TCP NetPacket::UDP NetPacket::ICMP;
+
+if [ $? -eq 0 ]
+then
+  echo "Done!"
+else
+  echo "Error installing CPAN modules, try to run this script again"
